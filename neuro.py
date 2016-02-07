d33573042d6e01442eb82808a2f44c71dff56545
gk <gabriel.kabbe@mail.de>
2016-02-08 00:03:50 +0100

    rename main file

--- neuro.py
+++ neuro.py
@@ -1,77 +1,92 @@
 #!/usr/bin/python
-import math,random
+import numpy as np
+from scipy.optimize import minimize as minimize
+import matplotlib.pylab as plt
+import ipdb
 
-class neurolayer:
-	def __init__(self, inputlayer, hiddenlayer_list, outputlayer):
-		self.inputlayer=[]
-		self.hiddenlayers=[]
-		self.outputlayer=[]
-		self.errorlayer=[]
-		self.trainingdata=[]
-		for i in range(inputlayer):
-			self.inputlayer.append(neuron())
-		for i in range(len(hiddenlayer_list)):
-			self.hiddenlayers.append([])
-		for i in range(hiddenlayer_list[0]):
-			self.hiddenlayers[0].append(neuron([1]*inputlayer, self.inputlayer))
-		for i in range(1,len(hiddenlayer_list)):
-			for j in range(neuronnumber[i]):
-				self.hiddenlayers[i].append(neuron([1]*hiddenlayer_list[i-1], self.hiddenlayers[i-1]))
-		for i in range(outputlayer):
-			self.outputlayer.append(neuron([1]*hiddenlayer, self.hiddenlayer))
-		for i in range(outputlayer):
-			self.errorlayer.append(neuron([1], self.outputlayer[i]))
-			
-	def setinput(self, inputlist):
-		self.inputlist=inputlist
-		for i in range(len(inputlist)):
-			self.inputlayer[i].value=inputlist[i]
-			
-	def calcoutput(self):
-		for layer in self.hiddenlayers:
-			for n in layer:
-				n.output()
-		for n in self.outputlayer:
-			n.output()
-	#trainingsdaten in form von dict {input:output}		
-	def addtrainingdata(self, data):
-		self.trainingdata.append(data)
-		
-	def backpropagation(self):
-		
-		
-		
-		
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
 
-class neuron:
-	def __init__(self, weights=None, ancestors=None, value=0):
-		self.weights=weights
-		if value==0:
-			self.value=random.random()
-		else:
-			self.value=value
-		if ancestors:
-			self.ancestors=ancestors
-	
-	def getinputs(self, ancestors):
-		self.ancestors=ancestors
-		
-	def fermithresh(self, x):
-		return 1./(math.exp(-x)+1)
-	
-	def fermideriv(self, x):
-		ex=math.exp(-x)
-		return ex/((ex+1)(ex+1))
-		
-	def output(self):
-		summe=0
-		if len(self.weights)!=len(self.ancestors):
-			raise Exception
-		for i in range(len(self.weights)):
-			summe+=self.ancestors[i].value*self.weights[i]
-		self.value=self.fermithresh(summe)
-		self.deriv=fermideriv(summe)
+class FeedForwardNetwork:
+    def __init__(self, layer_lengths, bias=True, hidden_activation=np.tanh, output_activation=lambda x:x):
+        self.inputlayer_length = layer_lengths[0]
+        self.hiddenlayer_lengths = layer_lengths[1:-1]
+        self.outputlayer_length = layer_lengths[-1]
+        self.layer_lengths = layer_lengths
 
-class errornode:
-	def __init__(self, ):
-		
+        self.bias = bias
+        self.hidden_activation = hidden_activation
+        self.output_activation = output_activation
+
+        self.weights = []
+        weight_input_hidden = np.random.random((self.inputlayer_length+bias, self.hiddenlayer_lengths[0]))
+        self.weights.append(weight_input_hidden)
+        for hl_prev, hl_next in zip(self.hiddenlayer_lengths[:-1], self.hiddenlayer_lengths[1:]):
+            hl_weight = np.random.random((hl_prev+bias, hl_next))
+            self.weights.append(hl_weight)
+        weight_hidden_output = np.random.random((self.hiddenlayer_lengths[-1]+bias, self.outputlayer_length))
+        self.weights.append(weight_hidden_output)
+
+    def _forward_prop(self, inputarr, weights):
+        for weight, layer_len in zip(weights, self.layer_lengths[:-2]):
+            inputarr = np.dot(inputarr, weight[:layer_len])
+            if self.bias:
+                inputarr += weight[-1]
+            inputarr = self.hidden_activation(inputarr)
+        output = np.dot(inputarr, weights[-1][:self.layer_lengths[-2]])
+        if self.bias:
+            output += weights[-1][-1]
+        output = self.output_activation(output)
+        return output
+
+    def sim(self, input):
+        return self._forward_prop(input, self.weights)
+
+    def calc_error(self, weights_array, inputs, outputs):
+        weights = self._unflatten(weights_array)
+        nn_output = self._forward_prop(inputs, weights)
+        error = ((nn_output - outputs)**2).mean()
+        return error
+
+    def _unflatten(self, weight_array):
+        weights = []
+        shape = self.inputlayer_length + self.bias, self.hiddenlayer_lengths[0]
+        weights.append(weight_array[:shape[0]*shape[1]].reshape(shape))
+        start_pos = shape[0]*shape[1]
+        for hl_prev, hl_next in zip(self.hiddenlayer_lengths[:-1], self.hiddenlayer_lengths[1:]):
+            shape = hl_prev+self.bias, hl_next
+            weights.append(weight_array[start_pos:start_pos+shape[0]*shape[1]].reshape(shape))
+            start_pos += shape[0]*shape[1]
+        shape = self.hiddenlayer_lengths[-1] + self.bias, self.outputlayer_length
+        weights.append(weight_array[start_pos:start_pos+shape[0]*shape[1]].reshape(shape))
+        return weights
+
+    def optimize(self, training_set):
+        inputs, outputs = training_set
+        weights = np.hstack([w.flatten() for w in self.weights])
+        results = minimize(fun=self.calc_error, x0=weights, args=(inputs, outputs), method='BFGS')
+        result = results["x"]
+        print "diff:", (weights - result).sum()
+        self.weights = self._unflatten(result)
+        return self.weights
+
+def test():
+    def f(x):
+        return np.sin(x)*np.exp(-x**2/100)
+    nn = FeedForwardNetwork([1, 5, 1])
+    inputs = np.linspace(-10, 10, 25)[:, None]
+    outputs = f(inputs)
+    # outputs = inputs**2
+    print inputs.shape
+    print outputs.shape
+    nn.optimize((inputs, outputs))
+
+    x = np.linspace(-15, 15, 100)
+    y = nn.sim(x[:, None])
+
+    plt.plot(x, f(x), "x")
+    plt.plot(x, y)
+    plt.show()
+
+if __name__ == "__main__":
+    test()
