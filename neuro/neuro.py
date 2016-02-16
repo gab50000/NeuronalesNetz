066922cb034cbfc945738cdaed5bd104c34be123
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-02-16 14:50:56 +0100

    Add method for chunked calculation of NN output
    For large inputs, the NN simulation becomes very memory intensive.
    This is circumvented by calculating the result in chunks.

--- neuro/neuro.py
+++ neuro/neuro.py
@@ -11,11 +11,11 @@ def sigmoid(x):
     np.power(x, -1)
     
 def identity(x):
-    pass
+    return x
 
 
 class FeedForwardNetwork:
-    def __init__(self, layer_lengths, bias=True, hidden_activation=np.tanh, output_activation=identity, verbose=False):
+    def __init__(self, layer_lengths, bias=True, hidden_activation=np.tanh, output_activation=identity, verbose=False, chunk_size=None):
         """A FeedForwardNetwork object is initialized by providing a list that contains the number of nodes for
         each layer.
         For example, a FFN object with an input layer with one node, a hidden layer with 5 nodes and an output layer
@@ -24,12 +24,15 @@ class FeedForwardNetwork:
             ffn = FeedForwardNetwork([1, 5, 2])
 
         Further (optional) parameters are
-          * bias (expects a boolean), which determines whether each layer receives an additional bias node
-          * hidden_activation, which determines the activation function for each hidden layer
-          * output_activation, which determines the activation function for the output layer"""
+          * bias (expects a boolean): each layer receives an additional bias node
+          * hidden_activation: determines the activation function for each hidden layer
+          * output_activation: determines the activation function for the output layer
+          * chunks: calculate the inputs in chunks. Useful for large input data.
+          """
         self.layer_lengths = layer_lengths
         self.bias = bias
         self.verbose = verbose
+        self.chunk_size = chunk_size
         # if bias is activated, the bias weights are left out from the matrix multiplication
         if self.bias:
             self.weight_slicer = slice(None, -1)
@@ -38,36 +41,59 @@ class FeedForwardNetwork:
 
         self.hidden_activation = hidden_activation
         self.output_activation = output_activation
+        
+        self.activation_fcts = [hidden_activation for layer in self.layer_lengths]
+        self.activation_fcts.append(self.output_activation)
 
         self.weights = []
         for hl_prev, hl_next in zip(self.layer_lengths[:-1], self.layer_lengths[1:]):
             hl_weight = np.random.random((hl_prev+bias, hl_next))
             self.weights.append(hl_weight)
+        
+        if self.chunk_size:
+            self._prop = self._forward_prop_chunked
+        else:
+            self._prop = self._forward_prop
+            
 
     def _forward_prop(self, input_array, weights):
         if self.verbose:
             print "forward prop"
-        for weight in weights[:-1]:
+        for weight, act_fct in zip(weights, self.activation_fcts):
+            # print "hidden"
             input_array = np.dot(input_array, weight[self.weight_slicer])
             if self.bias:
                 input_array += weight[-1]
-            self.hidden_activation(input_array)
-        output = np.dot(input_array, weights[-1][self.weight_slicer])
-        if self.bias:
-            output += weights[-1][-1]
-        self.output_activation(output)
-        return output
+            input_array = act_fct(input_array)
+        return input_array
 
+    def _forward_prop_chunked(self, input_array, weights):
+        if self.verbose:
+            print "forward prop chunked"
+        for weight, act_fct in zip(weights, self.activation_fcts):
+            temp_arr = np.zeros((input_array.shape[0], weight[self.weight_slicer].shape[1]), dtype=input_array.dtype)
+            for chunk in xrange(0, temp_arr.shape[0], self.chunk_size):
+                if chunk + self.chunk_size > temp_arr.shape[0]:
+                    sl = slice(chunk, None)
+                else:
+                    sl = slice(chunk+self.chunk_size)
+                np.dot(input_array[sl], weight[self.weight_slicer], out=temp_arr[sl])
+            input_array = temp_arr
+            if self.bias:
+                input_array += weight[-1]
+            input_array = act_fct(input_array)
+        return input_array
+        
     def sim(self, input_array):
         """Calculates the output for a given array of inputs.
         Expects an array of the shape (No. of inputs, No. of input nodes)"""
-        return self._forward_prop(input_array, self.weights)
+        return self._prop(input_array, self.weights)
 
     def _calc_error(self, weight_array, inputs, outputs):
         if self.verbose:
             print "error"
         weights = self._unflatten(weight_array)
-        nn_output = self._forward_prop(inputs, weights)
+        nn_output = self._prop(inputs, weights)
         error = ((nn_output - outputs)**2).mean()
         return error
 
@@ -101,18 +127,19 @@ class FeedForwardNetwork:
 def test():
     def f(x):
         return np.sin(x)*np.exp(-x**2/100)
-    nn = FeedForwardNetwork([1, 3, 1])
+    nn = FeedForwardNetwork([1, 12, 1], verbose=False, chunk_size=3)
     inputs = np.linspace(-10, 10, 25)[:, None]
     outputs = f(inputs)
     print inputs.shape
     print outputs.shape
     nn.optimize((inputs, outputs))
-
+    
     x = np.linspace(-15, 15, 100)
     y = nn.sim(x[:, None])
 
     plt.plot(x, f(x), "x")
-    plt.plot(x, y)
+    plt.plot(x, y, label="bla")
+    plt.legend(loc="upper left")
     plt.show()
 
 if __name__ == "__main__":
