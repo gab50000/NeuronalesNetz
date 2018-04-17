c018831155cbe3210e0fed189dad595fe33f2188
gk <gabriel.kabbe@mail.de>
2018-04-17 23:41:00 +0200

    Implement class which uses torch Modules

--- neuro/pytorch/test.py
+++ neuro/pytorch/test.py
@@ -30,6 +30,18 @@ class FeedForward(torch.nn.Module):
         self.b2.grad.data.zero_()
 
 
+class FeedForwardSuperior(torch.nn.Module):
+    def __init__(self, in_size, hidden_size, out_size):
+        super().__init__()
+        # torch.nn.Linear already contains bias
+        self.lin1 = torch.nn.Linear(in_size, hidden_size)
+        self.lin2 = torch.nn.Linear(hidden_size, out_size)
+
+    def forward(self, x):
+        hidden = torch.tanh(self.lin1(x))
+        return self.lin2(hidden)
+
+
 def main():
     x = Variable(torch.randn(1).type(torch.FloatTensor), requires_grad=True)
     print("x =", x.data[0])
@@ -40,11 +52,15 @@ def main():
     print("dy/dx =", x.grad.data[0])
 
 
-def ff_test():
+def get_data():
     x = np.linspace(0, 2 * np.pi)
     y = np.sin(x)
     x_t = Variable(torch.from_numpy(x).type(torch.FloatTensor), requires_grad=False)
     y_t = Variable(torch.from_numpy(y).type(torch.FloatTensor), requires_grad=False)
+    return x_t, y_t
+
+def ff_test():
+    x_t, y_t = get_data()
 
     ff = FeedForward(50, 20, 50)
     learning_rate = 1e-3
@@ -60,7 +76,28 @@ def ff_test():
         ff.set_grad_zero()
 
 
-    plt.plot(x, y_pred.data.numpy())
+    plt.plot(x_t.data.numpy(), y_pred.data.numpy())
+    plt.show()
+
+
+def ff_superior():
+    x_t, y_t = get_data()
+
+    ff = FeedForwardSuperior(50, 20, 50)
+    criterion = torch.nn.MSELoss(size_average=False)
+    optimizer = torch.optim.SGD(ff.parameters(), lr=1e-3)
+
+    for i in range(200):
+        y_pred = ff(x_t)
+
+        loss = criterion(y_pred, y_t)
+        print("Loss =", loss.data[0])
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    plt.plot(x_t.data.numpy(), y_pred.data.numpy())
     plt.show()
 
 
