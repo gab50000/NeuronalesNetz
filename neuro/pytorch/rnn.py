d47d457b0344a506f04e70a8480c9076a61bb40d
gk <gabriel.kabbe@mail.de>
2018-04-19 00:19:46 +0200

    Update sine example

--- neuro/pytorch/rnn.py
+++ neuro/pytorch/rnn.py
@@ -1,4 +1,5 @@
 import fire
+import numpy as np
 import torch
 from torch.autograd import Variable
 
@@ -34,12 +35,38 @@ def main():
         print("hidden =", hidden)
 
 
+def get_sine():
+    start = np.random.uniform(-10, 10)
+    width = np.random.uniform(0.1, 20)
+    x = torch.FloatTensor(torch.linspace(start, start + width))
+    y = torch.FloatTensor(torch.sin(x))
+    return x, y
+
+
 def learn_sine():
     in_size = 1
     hidden_size = 100
     out_size = 1
 
     rnn = RNN(in_size, hidden_size, out_size)
+    hidden = Variable(torch.randn(100))
+    criterion = torch.nn.MSELoss(size_average=False)
+    optimizer = torch.optim.SGD(rnn.parameters(), lr=1e-3)
+
+    for i in range(100):
+        xs, ys = get_sine()
+        loss = 0
+        for x, y_target in zip(xs, ys):
+            import ipdb; ipdb.set_trace()
+            print(x, y_target)
+            y_pred, hidden = rnn(x, hidden)
+            loss += criterion(y_target, y_pred)
+        print("Loss =", loss.data[0])
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
 
 
 fire.Fire()
